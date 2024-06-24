35b75ebe94273ed0ac820beb252a5d49cbc1d29f
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2024-06-24 19:54:37 +0330

    feat: models implemeted fo Dog, Cat, Breeder, DogBreed, CatBreed, Pet

---
+++ backend/models/models.go
@@ -0,0 +1,79 @@
+package models
+
+import "time"
+
+type DogBreed struct {
+	ID               int    `json:"id"`
+	Breed            string `json:"breed"`
+	WeightLosLbs     int    `json:"weight_los_lbs"`
+	WeightHighLbs    int    `json:"weight_high_lbs"`
+	AverageWeight    int    `json:"average_weight"`
+	LifeSpan         int    `json:"life_span"`
+	Details          string `json:"details"`
+	AlternateNames   string `json:"alternate_names"`
+	GeographicOrigin string `json:"geographic_origin"`
+}
+
+type Dog struct {
+	ID               int       `json:"id"`
+	DogName          string    `json:"dog_name"`
+	BreedId          int       `json:"breed_id"`
+	BreederId        int       `json:"breeder_id"`
+	Color            string    `json:"color"`
+	DateOfBirth      time.Time `json:"date_of_birth"`
+	SpayedOrNeutered bool      `json:"spayed_or_neutered"`
+	Description      string    `json:"description"`
+	Weight           int       `json:"weight"`
+	Breed            DogBreed  `json:"breed"`
+	Breeder          Breeder   `json:"breeder"`
+}
+
+type CatBreed struct {
+	ID               int    `json:"id"`
+	Breed            string `json:"breed"`
+	WeightLosLbs     int    `json:"weight_los_lbs"`
+	WeightHighLbs    int    `json:"weight_high_lbs"`
+	AverageWeight    int    `json:"average_weight"`
+	LifeSpan         int    `json:"life_span"`
+	Details          string `json:"details"`
+	AlternateNames   string `json:"alternate_names"`
+	GeographicOrigin string `json:"geographic_origin"`
+}
+
+type Cat struct {
+	ID               int       `json:"id"`
+	CatName          string    `json:"cat_name"`
+	BreedId          int       `json:"breed_id"`
+	BreederId        int       `json:"breeder_id"`
+	Color            string    `json:"color"`
+	DateOfBirth      time.Time `json:"date_of_birth"`
+	SpayedOrNeutered bool      `json:"spayed_or_neutered"`
+	Description      string    `json:"description"`
+	Weight           int       `json:"weight"`
+	Breed            CatBreed  `json:"breed"`
+	Breeder          Breeder   `json:"breeder"`
+}
+
+type Breeder struct {
+	ID          int         `'json:"id"`
+	BreederName string      `json:"breeder_name"`
+	Address     string      `json:"address"`
+	City        string      `json:"city"`
+	ProvState   string      `json:"prov_state"`
+	Country     string      `json:"country"`
+	Zip         string      `json:"zip"`
+	Phone       string      `json:"phone"`
+	Email       string      `json:"email"`
+	Active      int         `json:"active"`
+	DogBreeds   []*DogBreed `json:"dog_breeds"`
+	CatBreeds   []*CatBreed `json:"cat_breeds"`
+}
+
+type Pet struct {
+	Species     string `json:"species"`
+	Breed       string `json:"breed"`
+	MinWeight   int    `json:"min_weight"`
+	MaxWeight   int    `json:"max_weight"`
+	Description string `json:"description"`
+	LifeSpan    int    `json:"lifespan"`
+}
